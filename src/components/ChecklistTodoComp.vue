46aab1c841839b0daed7a2d62772fdd2d605c93c
BALICA Ionut <Ionut.BALICA@cegedim.com>
2022-08-09 17:19:43 +0300

    first commit

---
+++ src/components/ChecklistTodoComp.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="checklist">
+    <div v-for="item in todoList" :key="item">
+      <div v-if="item.isDone" >
+        <input type="checkbox" id="item.id" name="item.id" checked>
+        <label v-if="item.isDone"  for="item.id"><s>{{ item.textField }}</s></label>
+      </div>
+      <div v-else>
+        <input type="checkbox" id="item.id" name="item.id">
+        <label for="item.id">{{ item.textField }}</label>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ChecklistTodoComp',
+  components: {
+
+  },
+  props: ['text'],
+  setup() {
+    const todoList = [
+        {
+          id: 1, isDone: false, textField: "test 1", dateCreated: "1659447034", dueDate: ""
+        },
+        {
+          id: 2, isDone: true, textField: "test 2", dateCreated: "1659437034", dueDate: ""
+        },
+        {
+          id: 3, isDone: false, textField: "test 3", dateCreated: "1659427034", dueDate: "1659477034"
+        }
+      ]
+
+    const timestamp = 1659447034
+
+    var date = unixToDate(timestamp)
+
+    var unixTime = dateToUnix(date)
+
+    // var list = parseTodoList(todoList)
+
+    function unixToDate (data) {
+        data = new Date(data * 1000)
+      return data
+    }
+
+    function dateToUnix (data) {
+        data = data.getTime() / 1000
+      return data
+    }
+
+    // function parseTodoList(data) {
+    //   data = JSON.parse(data)
+    //   return data
+    // }
+
+
+
+    return {
+      date,
+      unixTime,
+      todoList,
+      // list
+    }
+  },
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
---
+++ src/views/ChecklistsView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="checklist">
+    <ChecklistTodoCompVue 
+      :text="text"
+    />
+  </div>
+</template>
+
+<script>
+
+import ChecklistTodoCompVue from '@/components/ChecklistTodoComp.vue'
+
+export default {
+  name: 'ChecklistsView',
+  components: {
+    ChecklistTodoCompVue,
+  },
+  data() {
+    return {
+        text: "To Do List"
+    }
+  }
+}
+</script>
